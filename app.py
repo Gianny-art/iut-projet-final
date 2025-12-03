0837a632d8683422f0920af54ce2d32f985dfa64
Gianny-Art <giannyfoapa@gmail.com>
2025-12-03 21:53:25 +0100

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -764,5 +764,6 @@ def place_bet():
     session['user']['balance'] = new_bal
     return jsonify({'ok': True, 'balance': new_bal})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
